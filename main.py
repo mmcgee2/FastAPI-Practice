cc411718b59d4f51004888c15f480af81acdc542
Michael <mcgeemike8@hotmail.com>
2022-06-28 10:35:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,3 @@ def read_user(user_id: str, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User Not Found")
     return db_user
-
-@app.get("/")
-def root():
-    return "message: No u"
